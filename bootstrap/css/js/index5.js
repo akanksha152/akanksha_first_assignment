5395a451d20f2367dfff9918a31a2407619afbe3
akanksharma152 <akanksharma152@gmail.com>
2016-11-14 17:33:36 +0530

    bootstrap_project

---
+++ bootstrap/css/js/index5.js
@@ -0,0 +1,4 @@
+$('.sign').click(function(function(e){
+       e.preventDefault();
+       $('.form1'.slideDown();)
+});
---
+++ bootstrap/index5.js
@@ -0,0 +1,7 @@
+
+$(function(){
+	$('#yes').click(function(e){
+		e.preventDefault();
+		$('#form1').slideDown();
+	});
+});
